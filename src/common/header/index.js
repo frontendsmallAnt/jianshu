8bddc82befa02350dc444c2df5467f45695ab532
zhuzhichao <1041832785@qq.com>
2019-01-25 17:32:41 +0800

    添加styled-components

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import Header from "./common/header"
 
-class App extends Component {
+class Header extends Component {
     render() {
         return (
             <div>
-                <Header />
+                hello world
             </div>
         );
     }
 }
 
-export default App;
+export default Header;
